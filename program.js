50f644be3ec53f02f6fcf705bb8eedfa24b2333a
Nomadjackalope <bendoo02@msn.com>
2015-09-03 17:11:32 -0700

    Created all the divs with js

--- program.js
+++ program.js
@@ -1,11 +1,21 @@
 function createLines() {
+	var calendar = document.getElementById('week');
 	
 	// Make 7 rows
 	for(var i = 0; i < 7; i++) {
+		var day = document.createElement("div");
+		day.setAttribute("id", "day" + i);
+        day.setAttribute("class", "day");
+		calendar.appendChild(day);
 		
 		// Make 27 column lines
-		for(var j = 0; j < 27; j++ ) {
+		for(var j = 0; j < 27; j++) {
 			// Add lines
+			var halfHour = document.createElement("div");
+            halfHour.setAttribute("id", "halfHour" + j);
+            halfHour.setAttribute("class", "halfHour");
+            halfHour.innerHTML=j + ", " + i;
+            day.appendChild(halfHour);
 		}
-	}
+	}	
 }
